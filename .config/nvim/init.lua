9aa7a81b8b80dd2dfd4bb48ed430ec12b3dc4713
aseedee <64454213+aseedee@users.noreply.github.com>
2022-01-17 02:45:13 +0300

    nvim: removed double sumneko-lua setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -123,7 +123,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require'cmp_nvim_lsp'.update_capabilities(capabilities)
 
-local servers = { 'clangd', 'sumneko_lua' }
+local servers = { 'clangd' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
         on_attach = on_attach,
