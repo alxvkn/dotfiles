b649956b788d972f1283b7c7dd877db58872269f
aseedee <64454213+aseedee@users.noreply.github.com>
2022-01-24 18:08:03 +0300

    nvim: changed gitsigns line blame position to eol

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -80,7 +80,7 @@ require'gitsigns'.setup {
     numhl = true,
     current_line_blame = true,
     current_line_blame_opts = {
-        virt_text_pos = 'right_align',
+        virt_text_pos = 'eol',
         delay = 500
     }
 }
