c3c755ef5953ec90d228db9a192ff9c4e4464f0e
aseedee <64454213+aseedee@users.noreply.github.com>
2022-02-11 23:08:02 +0300

    nvim: packer: use floating w/ with single border

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -1,6 +1,13 @@
 local packer = require 'packer'
 packer.reset()
-packer.init { compile_path = vim.fn.stdpath 'data' .. '/site/plugin/packer_compiled.lua' }
+packer.init {
+  compile_path = vim.fn.stdpath 'data' .. '/site/plugin/packer_compiled.lua',
+  display = {
+    open_fn = function()
+      return require('packer.util').float { border = 'single' }
+    end,
+  },
+}
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
 
