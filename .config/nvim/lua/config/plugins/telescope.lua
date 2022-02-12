2af4abd7d255842e99c6cd443ca4ae4f2081122e
aseedee <64454213+aseedee@users.noreply.github.com>
2022-02-13 01:46:55 +0300

    nvim: change telescope mappings prefix to 't'

--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -1,8 +1,8 @@
 local K = require 'config.util.map'
 
-K.n('<Leader>f<Leader>', [[<Cmd>lua require('telescope.builtin').builtin()<CR>]])
-K.n('<Leader>ff', [[<Cmd>lua require('telescope.builtin').find_files()<CR>]])
-K.n('<Leader>fg', [[<Cmd>lua require('telescope.builtin').live_grep()<CR>]])
-K.n('<Leader>fb', [[<Cmd>lua require('telescope.builtin').buffers()<CR>]])
-K.n('<Leader>fh', [[<Cmd>lua require('telescope.builtin').help_tags()<CR>]])
-K.n('<Leader>fj', [[<Cmd>lua require('telescope.builtin').planets()<CR>]])
+K.n('<Leader>t<Leader>', [[<Cmd>lua require('telescope.builtin').builtin()<CR>]])
+K.n('<Leader>tf', [[<Cmd>lua require('telescope.builtin').find_files()<CR>]])
+K.n('<Leader>tg', [[<Cmd>lua require('telescope.builtin').live_grep()<CR>]])
+K.n('<Leader>tb', [[<Cmd>lua require('telescope.builtin').buffers()<CR>]])
+K.n('<Leader>th', [[<Cmd>lua require('telescope.builtin').help_tags()<CR>]])
+K.n('<Leader>tj', [[<Cmd>lua require('telescope.builtin').planets()<CR>]])
