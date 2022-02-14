1cbe87be75752b082dedc799356ded3e86a98012
aseedee <64454213+aseedee@users.noreply.github.com>
2022-02-14 16:05:13 +0300

    nvim: disable cmp for buffer search and cmdline

--- .config/nvim/lua/config/plugins/cmp.lua
+++ .config/nvim/lua/config/plugins/cmp.lua
@@ -16,15 +16,3 @@ cmp.setup {
     { name = 'buffer' },
   },
 }
-cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' },
-  }, {
-    { name = 'cmdline' },
-  }),
-})
-cmp.setup.cmdline('/', {
-  sources = cmp.config.sources {
-    { name = 'buffer' },
-  },
-})
