cdf91358dad0a63db73247ff4372b2ca5bd4bede
aseedee <64454213+aseedee@users.noreply.github.com>
2022-09-09 01:40:25 +0300

    nvim: fix root dir detection for sumneko_lua to work in workspace mode with normal lua projects

--- .config/nvim/lua/config/plugins/lsp/nvim-lua-dev.lua
+++ .config/nvim/lua/config/plugins/lsp/nvim-lua-dev.lua
@@ -2,7 +2,12 @@ local luadev = require('lua-dev').setup {
   runtime_path = true,
   lspconfig = {
     on_attach = require 'config.plugins.lsp.on_attach',
-    root_dir = require('lspconfig').util.root_pattern 'init.lua',
+    root_dir = require('lspconfig').util.root_pattern(
+      'init.lua', --[[make root dir .config/nvim]]
+      '.luarc.json',
+      '.luacheckrc',
+      '.git'
+    ),
   },
 }
 require('lspconfig').sumneko_lua.setup(luadev)
