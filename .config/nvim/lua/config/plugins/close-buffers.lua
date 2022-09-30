26ac02c93fc14b5e83637e305d253dfa5795afb7
aseedee <64454213+aseedee@users.noreply.github.com>
2022-09-30 12:45:49 +0300

    nvim: bind to actual lua functions
    instead of :lua commands with lua code
    nvim 7.0+ i guess

--- .config/nvim/lua/config/plugins/close-buffers.lua
+++ .config/nvim/lua/config/plugins/close-buffers.lua
@@ -3,13 +3,13 @@ local K = require 'config.util.map'
 require('close_buffers').setup()
 
 -- BDelete
-K.n('<C-B>dh', [[<Cmd>lua require('close_buffers').delete { type = 'hidden' }<CR>]])
-K.n('<C-B>do', [[<Cmd>lua require('close_buffers').delete { type = 'other' }<CR>]])
-K.n('<C-B>da', [[<Cmd>lua require('close_buffers').delete { type = 'all' }<CR>]])
-K.n('<C-B>dd', [[<Cmd>lua require('close_buffers').delete { type = 'this' }<CR>]])
+K.n('<C-B>dh', function() require('close_buffers').delete { type = 'hidden' } end)
+K.n('<C-B>do', function() require('close_buffers').delete { type = 'other' } end)
+K.n('<C-B>da', function() require('close_buffers').delete { type = 'all' } end)
+K.n('<C-B>dd', function() require('close_buffers').delete { type = 'this' } end)
 
 -- BWipeout
-K.n('<C-B>wh', [[<Cmd>lua require('close_buffers').wipe { type = 'hidden' }<CR>]])
-K.n('<C-B>wo', [[<Cmd>lua require('close_buffers').wipe { type = 'other' }<CR>]])
-K.n('<C-B>wa', [[<Cmd>lua require('close_buffers').wipe { type = 'all' }<CR>]])
-K.n('<C-B>ww', [[<Cmd>lua require('close_buffers').wipe { type = 'this' }<CR>]])
+K.n('<C-B>wh', function() require('close_buffers').wipe { type = 'hidden' } end)
+K.n('<C-B>wo', function() require('close_buffers').wipe { type = 'other' } end)
+K.n('<C-B>wa', function() require('close_buffers').wipe { type = 'all' } end)
+K.n('<C-B>ww', function() require('close_buffers').wipe { type = 'this' } end)
--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -1,8 +1,8 @@
 local K = require 'config.util.map'
 
-K.n('<Leader>t<Leader>', [[<Cmd>lua require('telescope.builtin').builtin()<CR>]])
-K.n('<Leader>tf', [[<Cmd>lua require('telescope.builtin').find_files()<CR>]])
-K.n('<Leader>tg', [[<Cmd>lua require('telescope.builtin').live_grep()<CR>]])
-K.n('<Leader>tb', [[<Cmd>lua require('telescope.builtin').buffers()<CR>]])
-K.n('<Leader>th', [[<Cmd>lua require('telescope.builtin').help_tags()<CR>]])
-K.n('<Leader>tj', [[<Cmd>lua require('telescope.builtin').planets()<CR>]])
+K.n('<Leader>t<Leader>', function() require('telescope.builtin').builtin() end)
+K.n('<Leader>tf', function() require('telescope.builtin').find_files() end)
+K.n('<Leader>tg', function() require('telescope.builtin').live_grep() end)
+K.n('<Leader>tb', function() require('telescope.builtin').buffers() end)
+K.n('<Leader>th', function() require('telescope.builtin').help_tags() end)
+K.n('<Leader>tj', function() require('telescope.builtin').planets() end)
