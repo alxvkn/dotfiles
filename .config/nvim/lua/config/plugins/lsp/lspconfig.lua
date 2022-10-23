022b46734b0fe59d04609a922e0c035afeb731db
aseedee <64454213+aseedee@users.noreply.github.com>
2022-10-23 18:45:42 +0300

    nvim: lsp: cmp_nvim_lsp.update_capabilities() is being deprecated

--- .config/nvim/lua/config/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/config/plugins/lsp/lspconfig.lua
@@ -2,8 +2,7 @@ local lspconfig = require 'lspconfig'
 
 local on_attach = require 'config.plugins.lsp.on_attach'
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- stop all active clients if there are any
 -- (if config is reloaded)
