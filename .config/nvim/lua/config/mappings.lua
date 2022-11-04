77bcd23d7a23487b6d0e87986ffbc5272fd82514
aseedee <64454213+aseedee@users.noreply.github.com>
2022-11-05 02:38:36 +0300

    nvim: buffer switching mappings & remove <C-;> as <C-^>

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -1,7 +1,7 @@
 local K = require 'config.util.map'
 
--- ctrl+; as synonym to ctrl+^
-K.n('<C-;>', '<C-^>')
-
 -- go to normal mode from terminal with alt+[
 K.t('<A-[>', '<C-\\><C-N>')
+
+K.n('<C-j>', '<Cmd>bnext<CR>')
+K.n('<C-k>', '<Cmd>bprevious<CR>')
