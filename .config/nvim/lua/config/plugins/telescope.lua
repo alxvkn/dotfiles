a9807dc3e36ab85dfa4fb957605c3e2401f60ac9
aseedee <64454213+aseedee@users.noreply.github.com>
2022-12-21 04:12:32 +0300

    nvim: remap <leader>tg to telescope's git_files

--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -2,7 +2,7 @@ local K = require 'config.util.map'
 
 K.n('<Leader>t<Leader>', function() require('telescope.builtin').builtin() end)
 K.n('<Leader>tf', function() require('telescope.builtin').find_files() end)
-K.n('<Leader>tg', function() require('telescope.builtin').live_grep() end)
+K.n('<Leader>tg', function() require('telescope.builtin').git_files() end)
 K.n('<Leader>tb', function() require('telescope.builtin').buffers() end)
 K.n('<Leader>th', function() require('telescope.builtin').help_tags() end)
 K.n('<Leader>tj', function() require('telescope.builtin').planets() end)
