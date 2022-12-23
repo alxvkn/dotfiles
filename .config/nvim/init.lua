e78138e570f7ba2e6cc82c93818414e87711ed78
aseedee <64454213+aseedee@users.noreply.github.com>
2022-12-23 05:37:21 +0300

    nvim: set neovide transparency to 0.8

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -76,6 +76,7 @@ vim.o.guifont = 'Fira Code:h10'
 vim.o.title = true
 
 if vim.g.neovide then
+  vim.g.neovide_transparency = 0.8
   vim.g.neovide_cursor_vfx_mode = 'sonicboom'
   vim.g.neovide_cursor_animation_length = 0.15
   vim.g.neovide_cursor_trail_size = 0.3
