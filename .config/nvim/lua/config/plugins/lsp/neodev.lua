d2af15b7c0d778e18439bafb8d62b4acf8b7b682
aseedee <64454213+aseedee@users.noreply.github.com>
2022-12-23 05:53:38 +0300

    nvim: lsp: never run sumneko lua on home directory
    just to supress startup warnings

--- .config/nvim/lua/config/plugins/lsp/neodev.lua
+++ .config/nvim/lua/config/plugins/lsp/neodev.lua
@@ -1,12 +1,21 @@
 require('neodev').setup {}
-require('lspconfig').sumneko_lua.setup {
+local lspconfig = require 'lspconfig'
+
+lspconfig.sumneko_lua.setup {
   on_attach = require 'config.plugins.lsp.on_attach',
-  root_dir = require('lspconfig').util.root_pattern(
-    'init.lua', --[[make root dir .config/nvim]]
-    '.luarc.json',
-    '.luacheckrc',
-    '.git'
-  ),
+  root_dir = function(filename)
+    local dir = lspconfig.util.root_pattern(
+      'init.lua', --[[make root dir .config/nvim]]
+      '.luarc.json',
+      '.luacheckrc',
+      '.git'
+    )(filename)
+    -- run in single file mode if detected root dir is $HOME
+    if dir == os.getenv('HOME') then
+      dir = nil
+    end
+    return dir
+  end,
   settings = {
     Lua = {
       completion = {
