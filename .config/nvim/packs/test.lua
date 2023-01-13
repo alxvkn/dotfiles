d8a500ef88313721d59393199f3e9607d1e82ce3
aseedee <64454213+aseedee@users.noreply.github.com>
2023-01-13 03:34:18 +0300

    nvim: move plugin spec files to (config dir)/packs

--- .config/nvim/packs/test.lua
+++ .config/nvim/packs/test.lua
@@ -1,3 +1,4 @@
+print('lua/plugins/test.lua')
 local use = require('packer').use
 
 use 'wbthomason/packer.nvim'
@@ -14,7 +15,29 @@ use 'tpope/vim-sensible'
 use {
   'nvim-lualine/lualine.nvim',
   requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-  config = function() require('config.plugins.lualine') end,
+  config = function()
+    require('lualine').setup {
+      extensions = {
+        'toggleterm',
+        'fugitive',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = {
+          'encoding',
+          'fileformat',
+          {
+            'filetype',
+            icon_only = true,
+          },
+        },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+    }
+  end,
 }
 
 -- i would say, make netrw actually usable
--- .config/nvim/plugin/packer.lua
+++ .config/nvim/plugin/packer.lua
@@ -15,3 +15,15 @@ packer.init {
     end,
   },
 }
+
+-- plugin specification files
+-- (there is no way to require() files by glob, so we are sourcing them)
+-- for the future: i guess the whole thing is a little bit venturous and will cause problems
+-- but how cool is it when you add a plugin just by creating one new file?
+local plugin_spec_files = vim.fn.split(vim.fn.glob(vim.fn.stdpath('config') .. '/packs/*'), '\n')
+for _,file in ipairs(plugin_spec_files) do
+  print('sourcing file' .. file)
+  vim.cmd.source(file)
+end
+
+print('packer.lua end')
