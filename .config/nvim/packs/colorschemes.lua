8c3a79d663d87ef19b289a7ac0a175cec93e6eb3
aseedee <64454213+aseedee@users.noreply.github.com>
2023-01-17 04:10:21 +0300

    nvim: move colorschemes to separate file

---
+++ .config/nvim/packs/colorschemes.lua
@@ -0,0 +1,10 @@
+require('packer').use {
+  'RRethy/nvim-base16',
+  'dikiaap/minimalist',
+  'srcery-colors/srcery-vim',
+  'nanotech/jellybeans.vim',
+  'ntk148v/vim-horizon',
+  'ayu-theme/ayu-vim',
+  'kdheepak/monochrome.nvim',
+  'projekt0n/github-nvim-theme',
+}
--- .config/nvim/packs/test.lua
+++ .config/nvim/packs/test.lua
@@ -58,18 +58,6 @@ use {
   end,
 }
 
--- colorschemes
-use {
-  'RRethy/nvim-base16',
-  'dikiaap/minimalist',
-  'srcery-colors/srcery-vim',
-  'nanotech/jellybeans.vim',
-  'ntk148v/vim-horizon',
-  'ayu-theme/ayu-vim',
-  'kdheepak/monochrome.nvim',
-  'projekt0n/github-nvim-theme',
-}
-
 -- pywal integration
 use {
   'AlphaTechnolog/pywal.nvim',
