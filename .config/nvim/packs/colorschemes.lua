26eb883aeeaf1f5fc546194a5948b1137d9f84a2
on8t <64454213+on8t@users.noreply.github.com>
2023-01-28 04:29:58 +0300

    nvim: dump pywal, i'm tired
    my gf doesn't love me anymore i just i want to just i still love here
    idk what to do

--- .config/nvim/packs/colorschemes.lua
+++ .config/nvim/packs/colorschemes.lua
@@ -1,27 +1,4 @@
 require('packer').use {
-  {
-    -- pywal integration
-    'AlphaTechnolog/pywal.nvim',
-    config = function()
-      local f = io.open(os.getenv('HOME') .. '/.cache/wal/colors-wal.vim', 'r')
-      if f ~= nil then
-        f:close()
-        local ok, pywal = pcall(require, 'pywal')
-        if ok then
-          pywal.setup()
-          -- so bg overrides will happen
-          vim.cmd.doautocmd 'ColorScheme'
-        end
-        -- without that focused windows's status line will be filled with ^^^
-        vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'none' })
-      else
-        local _, _ = pcall(
-          require,
-          'nightfox'-- just so the config is loaded
-        )
-      end
-    end
-  },
   'RRethy/nvim-base16',
   'dikiaap/minimalist',
   'srcery-colors/srcery-vim',
@@ -32,7 +9,6 @@ require('packer').use {
   'projekt0n/github-nvim-theme',
   {
     'EdenEast/nightfox.nvim',
-    module = 'nightfox',
     config = function()
       print('nightfox\'s config function start')
       require('nightfox').setup {
