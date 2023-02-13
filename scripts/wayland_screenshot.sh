4e0b1f7b69a8024ffb094ab44b95ee5eaa10278d
on8t <64454213+on8t@users.noreply.github.com>
2023-02-13 17:29:16 +0300

    scripts: wayland_screenshot: add --no-save option

--- scripts/wayland_screenshot.sh
+++ scripts/wayland_screenshot.sh
@@ -5,18 +5,36 @@ SCRNFNAME="$SCRNDIR/$(date +%F.%H-%M-%S).png"
 
 mkdir -p $SCRNDIR
 
+# custom args will be added to this var
+GRIM_CMD="grim -t png"
+
+SAVE_TO_FILE=yes
+
 if [ $# -gt 0 ]; then
-    case $1 in
-        --area)
-            grim -g "$(slurp)" -t png $SCRNFNAME
-            ;;
-        *)
-            echo "usage: $0 [ --area ]"
-            exit 1
-            ;;
-    esac
-else
-    grim -t png $SCRNFNAME
+    while [ $# -gt 0 ]; do
+        case $1 in
+            --area)
+                GRIM_CMD="$GRIM_CMD -g \"$(slurp)\""
+                ;;
+            --no-save)
+                SAVE_TO_FILE=no
+                ;;
+            *)
+                echo "usage: $0 [ --area ] [ --no-save ]"
+                echo "    --no-save    don't save to file, only copy to clipboard"
+                exit 1
+                ;;
+        esac
+    shift
+    done
 fi
 
-wl-copy < $SCRNFNAME
+# append filename to GRIM_CMD to save
+if [ $SAVE_TO_FILE = 'yes' ]; then
+    GRIM_CMD="$GRIM_CMD $SCRNFNAME"
+    $GRIM_CMD
+    wl-copy < $SCRNFNAME
+else
+    GRIM_CMD="$GRIM_CMD -"
+    $GRIM_CMD | wl-copy
+fi
