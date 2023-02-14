30333445df4204072c6db4116f2054937ce9e1c2
on8t <64454213+on8t@users.noreply.github.com>
2023-02-14 20:13:39 +0300

    scripts: color picker

---
+++ scripts/wayland_color_picker.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+COLOR_FORMAT=${COLOR_FORMAT:-hex}
+echo $COLOR_FORMAT
+
+if [ $COLOR_FORMAT = rgb ]; then
+    CUT_FIELD=2
+else
+    CUT_FIELD=4
+fi
+
+wl-copy "$(grim -g "$(slurp -p)" -t png - |
+    convert - -format '%[pixel:p{0,0}]' txt:- | tail -1 | cut -d ' ' -f $CUT_FIELD)"
