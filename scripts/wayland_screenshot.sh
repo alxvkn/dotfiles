189707ec9236e75a9d29e9166937c11a1ea6f6ed
on8t <64454213+on8t@users.noreply.github.com>
2023-02-15 19:16:19 +0300

    scripts: screenshot: actually working

--- scripts/wayland_screenshot.sh
+++ scripts/wayland_screenshot.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 
-SCRNDIR="$HOME/.scrn"
-SCRNFNAME="$SCRNDIR/$(date +%F.%H-%M-%S).png"
+SCREENSHOT_DIR=${SCREENSHOT_DIR:-"$HOME/.scrn"}
 
-mkdir -p $SCRNDIR
-
-# custom args will be added to this var
 GRIM_CMD="grim -t png"
 
 SAVE_TO_FILE=yes
 
 if [ $# -gt 0 ]; then
     while [ $# -gt 0 ]; do
-        case $1 in
+        case "$1" in
             --area)
-                GRIM_CMD="$GRIM_CMD -g \"$(slurp)\""
+                SELECT_AREA=yes
                 ;;
             --no-save)
                 SAVE_TO_FILE=no
@@ -29,12 +25,13 @@ if [ $# -gt 0 ]; then
     done
 fi
 
-# append filename to GRIM_CMD to save
 if [ $SAVE_TO_FILE = 'yes' ]; then
-    GRIM_CMD="$GRIM_CMD $SCRNFNAME"
-    $GRIM_CMD
-    wl-copy < $SCRNFNAME
+    mkdir -p $SCREENSHOT_DIR
+    FILE_PATH="$SCREENSHOT_DIR/$(date +%F.%H-%M-%S).png"
+fi
+
+if [ $SELECT_AREA = 'yes' ]; then
+    $GRIM_CMD -g "$(slurp)" - | tee $FILE_PATH | wl-copy
 else
-    GRIM_CMD="$GRIM_CMD -"
-    $GRIM_CMD | wl-copy
+    $GRIM_CMD - | tee $FILE_PATH | wl-copy
 fi
