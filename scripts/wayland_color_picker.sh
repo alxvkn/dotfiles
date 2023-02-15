743940aac26dddc45970da09644f3246bcdbc0f0
on8t <64454213+on8t@users.noreply.github.com>
2023-02-15 04:59:09 +0300

    scripts: color picker: made working; removed format option
    i don't know shit about this imagemagick commands but now it works

--- scripts/wayland_color_picker.sh
+++ scripts/wayland_color_picker.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-COLOR_FORMAT=${COLOR_FORMAT:-hex}
+# will use a normal slurp selection, because on all compositors i've used
+# screenshot made with grim contains a cursor. so for this script to work you
+# will have to slightly move your mouse down or right (or both)
 
-if [ $COLOR_FORMAT = rgb ]; then
-    CUT_FIELD=2
-else
-    CUT_FIELD=4
-fi
-
-wl-copy "$(grim -g "$(slurp -p)" -t png - |
-    convert - -format '%[pixel:p{0,0}]' txt:- | tail -1 | cut -d ' ' -f $CUT_FIELD)"
+grim -g "$(slurp -b 00000000)" -t png - |
+    convert - -format '%[hex:p{0,0}]' info: | wl-copy
