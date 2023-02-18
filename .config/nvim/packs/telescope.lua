b5911fc492d08331593cfb7cc9777068603db3c9
on8t <64454213+on8t@users.noreply.github.com>
2023-02-18 20:53:30 +0300

    nvim: change most of telescope mappings to j prefix
    and add descriptions to some mappings

--- .config/nvim/packs/telescope.lua
+++ .config/nvim/packs/telescope.lua
@@ -3,14 +3,14 @@ require('packer').use {
   config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<Leader>t<Leader>', builtin.builtin)
-    vim.keymap.set('n', '<Leader>tf', builtin.find_files)
-    vim.keymap.set('n', '<Leader>tg', builtin.git_files)
-    vim.keymap.set('n', '<Leader>tb', builtin.buffers)
-    vim.keymap.set('n', '<Leader>th', builtin.help_tags)
-    vim.keymap.set('n', '<Leader>tj', builtin.planets)
+    vim.keymap.set('n', '<Leader>jf', builtin.find_files, { desc = '[J]ump to [F]ile' })
+    vim.keymap.set('n', '<Leader>jg', builtin.git_files, { desc = '[J]ump to [G]it file' })
+    vim.keymap.set('n', '<Leader>jb', builtin.buffers, { desc = '[J]ump to [B]uffer' })
+    vim.keymap.set('n', '<Leader>jh', builtin.help_tags, { desc = '[J]ump to [H]elp tag' })
+    vim.keymap.set('n', '<Leader>jj', builtin.planets)
 
-    vim.keymap.set('n', '<Leader>ds', builtin.lsp_document_symbols)
-    vim.keymap.set('n', '<Leader>ws', builtin.lsp_workspace_symbols)
+    vim.keymap.set('n', '<Leader>ds', builtin.lsp_document_symbols, { desc = 'Jump to [D]ocument [S]ymbol' })
+    vim.keymap.set('n', '<Leader>ws', builtin.lsp_workspace_symbols, { desc = 'Jump to [W]orkspace [S]ymbol' })
 
     vim.keymap.set('n', '<Leader>tc', function() builtin.colorscheme { enable_preview = true } end)
   end,
