d8a22c4f375de4e022fac3864a47f2619248377c
on8t <64454213+on8t@users.noreply.github.com>
2023-02-19 15:56:19 +0300

    nvim: some oil options

--- .config/nvim/packs/oil.lua
+++ .config/nvim/packs/oil.lua
@@ -1,7 +1,15 @@
 require('packer').use {
   'stevearc/oil.nvim',
   config = function()
-    require('oil').setup {}
+    require('oil').setup {
+      win_options = {
+        signcolumn = "yes"
+      },
+      keymaps = {
+        ['H'] = 'actions.parent',
+        ['L'] = 'actions.select'
+      }
+    }
     vim.keymap.set('n', '-', require('oil').open, {})
   end
 }
