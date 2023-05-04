bf175db25f274ac89c4f5bae2db93f9812ec5e3a
on8t <64454213+on8t@users.noreply.github.com>
2023-05-04 11:16:15 +0300

    nvim: add treesj

---
+++ .config/nvim/packs/treesj.lua
@@ -0,0 +1,9 @@
+require('packer').use {
+  'Wansmer/treesj',
+  config = function ()
+    require('treesj').setup {
+      use_default_keymaps = false,
+      vim.keymap.set('n', '<Leader>m', '<Cmd>TSJToggle<cr>')
+    }
+  end
+}
