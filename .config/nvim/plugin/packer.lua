19b924dcd39fb91a90291f95db162d7aa197ab2d
on8t <64454213+on8t@users.noreply.github.com>
2023-05-12 16:53:18 +0300

    nvim: use vim.loop.fs_stat to check if packer is installed

--- .config/nvim/plugin/packer.lua
+++ .config/nvim/plugin/packer.lua
@@ -1,6 +1,6 @@
 -- installing packer.nvim if it isn't already installed
 local packer_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(packer_path)) ~= 0 then
+if not vim.loop.fs_stat(packer_path) then
   require('install_packer').install(packer_path)
   return
   -- this file will re-executed by the install function,
