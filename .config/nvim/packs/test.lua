e3cd850ad538b0d4d50ee73c2741628a1728ed46
on8t <64454213+on8t@users.noreply.github.com>
2023-05-12 22:54:46 +0300

    nvim: add nvim-treesitter-endwise

--- .config/nvim/packs/test.lua
+++ .config/nvim/packs/test.lua
@@ -23,6 +23,21 @@ use {
     require('nvim-surround').setup {}
   end,
 }
+
+-- auto complete language-specific constructions
+-- like if then/end, function()/end in lua
+use {
+  'RRethy/nvim-treesitter-endwise',
+  after = 'nvim-treesitter',
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      endwise = {
+        enable = true
+      }
+    }
+  end
+}
+
 -- properly repeat surround mappings with .
 use 'tpope/vim-repeat'
 
