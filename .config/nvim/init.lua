62dfa176cd8f40809f1679d31bf0960b57786532
on8t <64454213+on8t@users.noreply.github.com>
2023-05-12 22:32:21 +0300

    nvim: map kj as <esc>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -44,6 +44,8 @@ vim.keymap.set('t', '<A-[>', '<C-\\><C-N>')
 vim.keymap.set('n', '<C-j>', '<Cmd>bnext<CR>')
 vim.keymap.set('n', '<C-k>', '<Cmd>bprevious<CR>')
 
+vim.keymap.set('i', 'kj', '<Esc>')
+
 vim.keymap.set({'n', 'v'}, '<Leader>y', '"+y', { desc = 'Yank to system clipboard' })
 vim.keymap.set({'n', 'v'}, '<Leader>p', '"+p', { desc = 'Paste from system clipboard' })
 
