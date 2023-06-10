b4c89e33d1a6d136bcf84bce9ee1767c11faffc3
on8t <64454213+on8t@users.noreply.github.com>
2023-06-10 03:38:43 +0300

    nvim: i forgor something💀💀💀

---
+++ .config/nvim/lua/colors.lua
@@ -0,0 +1,17 @@
+if os.getenv('TERM') ~= 'linux' then
+  vim.o.termguicolors = true
+end
+
+
+-- vim.api.nvim_create_autocmd('ColorScheme', {
+--   group = vim.api.nvim_create_augroup('transparent_bg', {}),
+--   callback = function()
+--     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+--     vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+--     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+--     vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+--     vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = 'none' })
+--     vim.api.nvim_set_hl(0, 'TelescopeBorder', { bg = 'none' })
+--     require('lualine').setup {}
+--   end
+-- })
---
+++ .config/nvim/lua/lazy_bootstrap.lua
@@ -0,0 +1,34 @@
+-- installing lazy.nvim if it isn't already installed
+local lazy_path = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazy_path) then
+  print('cloning lazy to ' .. lazy_path)
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim',
+    '--branch=stable',
+    lazy_path,
+  })
+end
+
+vim.opt.rtp:prepend(lazy_path)
+
+local plugins = {}
+
+local plugin_spec_files = vim.fn.split(vim.fn.glob(vim.fn.stdpath('config') .. '/packs/*'), '\n')
+for _, file in ipairs(plugin_spec_files) do
+  table.insert(plugins, dofile(file))
+end
+
+-- vim.print(plugins)
+
+require('lazy').setup(plugins)
+
+vim.keymap.set('n', '<leader>lr',
+  function() vim.cmd.runtime 'plugin/lazy.lua' end,
+  { desc = 'Reload lazy plugin specifications' })
+
+vim.keymap.set('n', '<leader>ll',
+  vim.cmd.Lazy,
+  { desc = 'Open lazy.nvim' })
