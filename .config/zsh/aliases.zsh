d55d2539f64bb2c7c8b681ae7cdccbf0c90524ea
on8t <64454213+on8t@users.noreply.github.com>
2023-06-26 21:45:29 +0300

    zsh: forgor some

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,23 @@
+# more of a default flags
+alias ls='ls -Fh --color=auto'
+
+## make less interpret color sequences
+alias less='less -R'
+
+## human readable sizes
+alias df='df -h'
+
+# actual aliases
+alias ll='ls -l'
+
+# shortcuts
+alias n='nvim'
+alias e="$EDITOR"
+alias g='git'
+alias t='tmux'
+
+alias fuck='doas $(fc -ln -1)'
+
+alias r="source $ZDOTDIR/.zshrc"
+
+source $ZDOTDIR/p.zsh
