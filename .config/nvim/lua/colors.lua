f902a558b7a44a68dab97962249b6094c734fa51
on8t <64454213+on8t@users.noreply.github.com>
2023-07-17 21:42:41 +0300

    nvim: funny transparent mode

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -1,12 +1,29 @@
--- vim.api.nvim_create_autocmd('ColorScheme', {
---   group = vim.api.nvim_create_augroup('transparent_bg', {}),
---   callback = function()
---     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
---     vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
---     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
---     vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
---     vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = 'none' })
---     vim.api.nvim_set_hl(0, 'TelescopeBorder', { bg = 'none' })
---     require('lualine').setup {}
---   end
--- })
+vim.keymap.set('n', '<Leader>tr', function()
+  -- load existing highlights and only change background
+  -- some goofy rocket science just for transparent shit 🤠
+  local highlight_names = {
+    'Normal',
+    'NormalNC',
+    'NormalFloat',
+    'FloatBorder',
+
+    'GitSignsAdd',
+    'GitSignsChange',
+    'GitSignsDelete',
+
+    'TelescopeNormal',
+
+    -- silly but i don't want to know how to
+    -- customize lualine theme it's hard
+    'lualine_c_normal',
+    'lualine_c_insert',
+    'lualine_c_visual',
+    'lualine_c_command',
+  }
+
+  for _, name in ipairs(highlight_names) do
+    local hl = vim.api.nvim_get_hl(0, { name = name })
+    hl.bg = 'none'
+    vim.api.nvim_set_hl(0, name, hl)
+  end
+end)
