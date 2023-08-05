e3c49000c256a9794e76d1d4558338e889e0f627
on8t <64454213+on8t@users.noreply.github.com>
2023-08-05 04:58:18 +0300

    nvim: lazy load nightfox, select rose-pine in it's setup

--- nvim/.config/nvim/packs/colorschemes.lua
+++ nvim/.config/nvim/packs/colorschemes.lua
@@ -2,21 +2,27 @@ return {
   'RRethy/nvim-base16',
   'dikiaap/minimalist',
   'ntk148v/vim-horizon',
-  'rose-pine/neovim',
   'ayu-theme/ayu-vim',
   'kdheepak/monochrome.nvim',
   'projekt0n/github-nvim-theme',
   {
-    'EdenEast/nightfox.nvim',
+    'rose-pine/neovim',
     config = function()
+      -- TODO: require this module somewhere else
       require 'colors'
+      vim.cmd.colorscheme 'rose-pine'
+    end
+  },
+  {
+    'EdenEast/nightfox.nvim',
+    lazy = true,
+    config = function()
       require('nightfox').setup {
         options = {
           dim_inactive = true,
           -- transparent = true
         }
       }
-      vim.cmd.colorscheme 'rose-pine'
     end
   },
 }
