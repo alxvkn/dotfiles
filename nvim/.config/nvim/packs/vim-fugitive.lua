3aa79be982d7c38ddd9e2247dedf36939ae09930
on8t <64454213+on8t@users.noreply.github.com>
2023-08-30 16:09:01 +0300

    nvim: add bindings to jump to git hunks

--- nvim/.config/nvim/packs/vim-fugitive.lua
+++ nvim/.config/nvim/packs/vim-fugitive.lua
@@ -9,7 +9,10 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     config = function()
-      require('gitsigns').setup {
+      local gitsigns = require('gitsigns')
+      vim.keymap.set('n', '[g', gitsigns.prev_hunk)
+      vim.keymap.set('n', ']g', gitsigns.next_hunk)
+      gitsigns.setup {
         signcolumn = false,
         numhl = true,
       }
