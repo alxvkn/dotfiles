485d0d3905b5fd46ec883c3cae60915022973a34
on8t <64454213+on8t@users.noreply.github.com>
2023-09-23 00:19:01 +0300

    nvim: neorg: change workspaces & new college location

--- nvim/.config/nvim/packs/neorg.lua
+++ nvim/.config/nvim/packs/neorg.lua
@@ -4,6 +4,7 @@ return {
   keys = {
     '<Leader>n',
     '<Leader>N',
+    '<Leader><Leader>w',
   },
   config = function()
     require('neorg').setup {
@@ -25,7 +26,7 @@ return {
             default_workspace = 'notes',
             workspaces = {
               notes = '~/documents/neorg/notes',
-              college = '~/documents/neorg/college'
+              college = '~/documents/college/neorg'
             }
           }
         }
@@ -33,5 +34,7 @@ return {
     }
     vim.keymap.set('n', '<Leader>n', vim.cmd['Neorg'])
     vim.keymap.set('n', '<Leader>N', ':Neorg ')
+    vim.keymap.set('n', '<Leader><Leader>wc', '<cmd>Neorg workspace college<cr>')
+    vim.keymap.set('n', '<Leader><Leader>wn', '<cmd>Neorg workspace notes<cr>')
   end,
 }
