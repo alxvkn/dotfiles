c4d00c05b5ebc440902933cdb559aec7fd1ae65a
on8t <64454213+on8t@users.noreply.github.com>
2023-11-06 01:19:44 +0300

    nvim: gitsigns: preview hunk binding + signcolumn

--- nvim/.config/nvim/packs/gitsigns.lua
+++ nvim/.config/nvim/packs/gitsigns.lua
@@ -4,9 +4,9 @@ return {
     local gitsigns = require('gitsigns')
     vim.keymap.set('n', '[g', gitsigns.prev_hunk)
     vim.keymap.set('n', ']g', gitsigns.next_hunk)
+    vim.keymap.set('n', '<Leader>gp', gitsigns.preview_hunk)
     gitsigns.setup {
-      signcolumn = false,
-      numhl = true,
+      signcolumn = true,
     }
   end,
 }
