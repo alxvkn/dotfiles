d375b13527b460ff224dbc5b9b04b466a56c1c27
on8t <64454213+on8t@users.noreply.github.com>
2024-01-05 09:30:48 +0300

    nvim: nvim_context_vt: disable virtual lines

--- nvim/.config/nvim/packs/nvim_context_vt.lua
+++ nvim/.config/nvim/packs/nvim_context_vt.lua
@@ -1,6 +1,9 @@
 return {
   'https://github.com/andersevenrud/nvim_context_vt',
   config = function()
-    require('nvim_context_vt').setup {}
+    require('nvim_context_vt').setup {
+      prefix = '',
+      disable_virtual_lines = true,
+    }
   end
 }
