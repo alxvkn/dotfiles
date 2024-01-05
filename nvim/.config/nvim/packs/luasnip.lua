1e5dcb0ee3999103ccb7dea39cf0e31ffb07b3f3
on8t <64454213+on8t@users.noreply.github.com>
2024-01-05 09:29:37 +0300

    nvim: luasnip: friendly-snippets as dependency & expand snippet with <c-s>

--- nvim/.config/nvim/packs/luasnip.lua
+++ nvim/.config/nvim/packs/luasnip.lua
@@ -1,7 +1,9 @@
 -- snippets
 return {
   'L3MON4D3/LuaSnip', -- engine
+  dependencies = { 'rafamadriz/friendly-snippets' },
   config = function()
+    vim.keymap.set('i', '<C-s>', require('luasnip').expand)
     require('luasnip').setup {}
     require('luasnip.loaders.from_vscode').lazy_load {
       exclude = { 'html', 'css' }
