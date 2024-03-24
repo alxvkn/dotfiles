61337919746626a20f3c8b08568d01e5e69673cb
on8t <64454213+on8t@users.noreply.github.com>
2024-03-24 17:00:01 +0300

    nvim: update todo.txt location for keybinding

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -85,7 +85,7 @@ vim.keymap.set('n', '0', '^')
 vim.keymap.set({'n', 'v'}, '<Leader>y', '"+y', { desc = 'Yank to system clipboard' })
 vim.keymap.set({'n', 'v'}, '<Leader>p', '"+p', { desc = 'Paste from system clipboard' })
 
-vim.keymap.set('n', '<Leader>T', '<Cmd>e ~/documents/college/todo.txt<cr>')
+vim.keymap.set('n', '<Leader>T', '<Cmd>e ~/Documents/obsidian/notes/todo.txt<cr>')
 
 vim.o.guifont = 'Iosevka Medium:h11'
 
