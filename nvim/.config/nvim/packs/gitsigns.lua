700b92e01e53dc4fa97357f91994f781bc3c5fec
on8t <64454213+on8t@users.noreply.github.com>
2024-04-14 17:34:24 +0300

    nvim: gitsigns: add keymappings for git actions (stage hunk, file, undo)

--- nvim/.config/nvim/packs/gitsigns.lua
+++ nvim/.config/nvim/packs/gitsigns.lua
@@ -5,6 +5,10 @@ return {
     vim.keymap.set('n', '[g', gitsigns.prev_hunk)
     vim.keymap.set('n', ']g', gitsigns.next_hunk)
     vim.keymap.set('n', '<Leader>gp', gitsigns.preview_hunk)
+    vim.keymap.set('n', '<Leader>gs', gitsigns.stage_hunk)
+    vim.keymap.set('n', '<Leader>gu', gitsigns.undo_stage_hunk)
+    vim.keymap.set('n', '<Leader>ga', gitsigns.stage_buffer)
+    vim.keymap.set('n', '<Leader>gr', gitsigns.reset_buffer)
     gitsigns.setup {
       signcolumn = true,
     }
