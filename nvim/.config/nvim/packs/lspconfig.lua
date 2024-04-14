f938848133d22f8cb5f1dd3e71a8e953745b5dfb
on8t <64454213+on8t@users.noreply.github.com>
2024-04-14 19:10:20 +0300

    nvim: lspconfig: server configs refactor

--- nvim/.config/nvim/packs/lspconfig.lua
+++ nvim/.config/nvim/packs/lspconfig.lua
@@ -13,60 +13,59 @@ return {
 
     -- servers with default settings
     local servers = {
-      'clangd',
-      'bashls',
-      'pyright',
-      'rust_analyzer',
-      'dartls',
-      'emmet_ls',
-      'html',
-      'cssls',
+      { 'clangd' },
+      { 'bashls' },
+      { 'pyright' },
+      { 'rust_analyzer' },
+      { 'dartls' },
+      { 'emmet_ls' },
+      { 'html' },
+      { 'cssls' },
+      { 'denols',
+        root_dir = lspconfig.util.root_pattern('deno.json', 'deno.jsonc'),
+        on_attach = function(client, bufnr)
+          if lspconfig.util.root_pattern('package.json')(vim.fn.getcwd()) then
+            client.stop()
+          end
+          on_attach(client, bufnr)
+        end,
+      },
+      { 'tsserver',
+        on_attach = function(client, bufnr)
+          if lspconfig.util.root_pattern('deno.json', 'deno.jsonc')(vim.fn.getcwd()) then
+            client.stop()
+          end
+          on_attach(client, bufnr)
+        end,
+        filetypes = {
+          'javascript',
+          'javascriptreact',
+          'javascript.jsx',
+          'typescript',
+          'typescriptreact',
+          'typescript.tsx',
+          'svelte',
+        },
+        cmd = { 'npx', 'typescript-language-server', '--stdio' },
+      },
+      { 'svelte',
+        cmd = { 'npx', 'svelte-language-server', '--stdio' },
+      },
     }
     for _, lsp in ipairs(servers) do
-      lspconfig[lsp].setup {
+      local name = lsp[1]
+      local config = {
         on_attach = on_attach,
         capabilities = capabilities,
       }
+
+      for k, v in pairs(lsp) do
+        if type(k) ~= 'number' then
+          config[k] = v
+        end
+      end
+
+      lspconfig[name].setup(config)
     end
-    -- servers that need some extra configuration
-    lspconfig.denols.setup {
-      root_dir = lspconfig.util.root_pattern('deno.json', 'deno.jsonc'),
-      on_attach = function(client, bufnr)
-        if lspconfig.util.root_pattern('package.json')(vim.fn.getcwd()) then
-          client.stop()
-        end
-        on_attach(client, bufnr)
-      end,
-      capabilities = capabilities,
-    }
-    lspconfig.tsserver.setup {
-      on_attach = function(client, bufnr)
-        if lspconfig.util.root_pattern('deno.json', 'deno.jsonc')(vim.fn.getcwd()) then
-          client.stop()
-        end
-        on_attach(client, bufnr)
-      end,
-      capabilities = capabilities,
-      filetypes = {
-        'javascript',
-        'javascriptreact',
-        'javascript.jsx',
-        'typescript',
-        'typescriptreact',
-        'typescript.tsx',
-        'svelte',
-      },
-      cmd = { 'npx', 'typescript-language-server', '--stdio' },
-    }
-    lspconfig.svelte.setup {
-      on_attach = on_attach,
-      capabilities = capabilities,
-      cmd = { 'npx', 'svelte-language-server', '--stdio' },
-    }
-    lspconfig.jsonls.setup {
-      on_attach = on_attach,
-      capabilities = capabilities,
-      cmd = { 'vscode-json-languageserver', '--stdio' },
-    }
   end,
 }
