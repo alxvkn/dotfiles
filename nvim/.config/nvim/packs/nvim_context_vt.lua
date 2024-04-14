960084fb48a8ccbfb57b6ae8cce956f57d7bf02f
on8t <64454213+on8t@users.noreply.github.com>
2024-04-14 17:41:12 +0300

    nvim: nvim_context_vt: custom highlight, new symbol, min_rows = 10

--- nvim/.config/nvim/packs/nvim_context_vt.lua
+++ nvim/.config/nvim/packs/nvim_context_vt.lua
@@ -1,9 +1,11 @@
 return {
   'https://github.com/andersevenrud/nvim_context_vt',
   config = function()
+    vim.cmd[[hi ContextVt ctermbg=black ctermfg=gray guibg=black guifg=gray cterm=italic gui=italic]]
     require('nvim_context_vt').setup {
-      prefix = '',
+      prefix = '󱞿',
       disable_virtual_lines = true,
+      min_rows = 10,
     }
   end
 }
