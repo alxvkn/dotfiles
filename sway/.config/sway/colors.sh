1bb3c2d75b4b19551bc4e7e3a27e5d4da7b839e9
on8t <64454213+on8t@users.noreply.github.com>
2024-05-05 21:58:35 +0300

    sway & i3blocks: define colors in colors.sh + dark mode integration

---
+++ sway/.config/sway/colors.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+bg=#0C0C10
+fg=#ffffff
+inactive_fg=#444444
+accent=#ffaaff
+
+if [ "$(gsettings get org.gnome.desktop.interface color-scheme)" = "'prefer-light'" ]; then
+    _bg=$bg
+    bg=$fg
+    fg=$_bg
+fi
+
+{
+    #       class                    border  bg      text    indicator   child_border
+    swaymsg "client.focused          $fg     $bg     $fg     #aaaaaa     #aaaaaa"
+    swaymsg "client.focused_inactive #000000 $bg     #aaaaaa"
+    swaymsg "client.unfocused        #000000 $bg     #aaaaaa             #000000"
+
+
+    swaymsg bar main colors "background          $bg"
+    swaymsg bar main colors "statusline          $fg"
+    swaymsg bar main colors "separator           $inactive_fg"
+
+    #                                            border          bg      text
+    swaymsg bar main colors "focused_workspace   $fg             $fg     $bg"
+    swaymsg bar main colors "inactive_workspace  $inactive_fg    $bg     $inactive_fg"
+    swaymsg bar main colors "urgent_workspace    $inactive_fg    $bg     #ffaaaa"
+} 2>&1 > /dev/null
