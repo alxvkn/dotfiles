0200e1625f279f9eaa1d78a26e4c885996c2cd90
on8t <64454213+on8t@users.noreply.github.com>
2024-05-06 00:13:48 +0300

    nvim: add folke/twilight.nvim

---
+++ nvim/.config/nvim/packs/twilight.lua
@@ -0,0 +1,8 @@
+return {
+  'folke/twilight.nvim',
+  config = function()
+    local twilight = require('twilight')
+    twilight.setup()
+    vim.keymap.set('n', '<Leader>t', twilight.toggle)
+  end,
+}
