626f5f8e0aa549e04623877df0e7b5708d23252a
on8t <64454213+on8t@users.noreply.github.com>
2024-05-25 15:14:54 +0300

    lf: previewer + enter to open

---
+++ lf/.config/lf/previewer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# ffmpeg -i Videos/bookpro.mp4 -frames:v 1 -q:v 5 -f mjpeg - | chafa -f sixel -
+
+case "$(file -Lb --mime-type -- "$1")" in
+    image/*)
+        chafa -f sixel -s "$2x$3" --animate off --polite on "$1"
+        exit 1
+        ;;
+    application/vnd.sqlite3)
+        sqlite3 "$1" .tables
+        exit 1
+        ;;
+    *)
+        if [ -n "$(command -v bat)" ]; then
+            bat -f --style=numbers "$1"
+        else
+            cat "$1"
+        fi
+        ;;
+esac
