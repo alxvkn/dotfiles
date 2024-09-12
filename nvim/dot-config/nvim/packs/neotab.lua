4cfacedb2a482d2ceeb41ef5aadab00a3e492cf4
on8t <64454213+on8t@users.noreply.github.com>
2024-09-12 23:17:21 +0300

    nvim: neotab: disable smart punctuators

--- nvim/dot-config/nvim/packs/neotab.lua
+++ nvim/dot-config/nvim/packs/neotab.lua
@@ -1,22 +1,6 @@
 return {
   'kawre/neotab.nvim',
   config = function()
-    require('neotab').setup {
-      smart_punctuators = {
-        enabled = true,
-        escape = {
-          enabled = true,
-          triggers = {
-            [','] = {
-              pairs = {
-                { open = "'", close = "'" },
-                { open = '"', close = '"' },
-              },
-              format = '%s', -- ", "
-            },
-          }
-        }
-      },
-    }
+    require('neotab').setup {}
   end
 }
