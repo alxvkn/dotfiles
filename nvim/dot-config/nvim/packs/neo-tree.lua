2e0425ab8cc3c31bf1bb5ab8aa92d1c4f052fbed
on8t <64454213+on8t@users.noreply.github.com>
2024-11-24 10:07:37 +0300

    nvim: neo-tree: toggle binding

--- nvim/dot-config/nvim/packs/neo-tree.lua
+++ nvim/dot-config/nvim/packs/neo-tree.lua
@@ -7,6 +7,6 @@ return {
     '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    vim.keymap.set('n', '<Leader>e', ':Neotree<cr>')
+    vim.keymap.set('n', '<Leader>e', ':Neotree toggle<cr>')
   end
 }
