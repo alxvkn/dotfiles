b4f9da9a8123f0a4cce2ff65ecda81d52ff90673
alxvkn <64454213+alxvkn@users.noreply.github.com>
2025-01-02 14:43:48 +0300

    nvim: some deprecated shit in plugin config

--- nvim/dot-config/nvim/packs/nvim-various-textobjs.lua
+++ nvim/dot-config/nvim/packs/nvim-various-textobjs.lua
@@ -1,7 +1,9 @@
 return {
   'chrisgrieser/nvim-various-textobjs',
   lazy = false,
-  config = function()
-    require('various-textobjs').setup { useDefaultKeymaps = true }
-  end,
+  opts = {
+    keymaps = {
+      useDefaults = true
+    }
+  }
 }
