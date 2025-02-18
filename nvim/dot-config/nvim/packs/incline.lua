fa424f189e16bef2f0799a3ab5fbce5345cf546a
alxvkn <64454213+alxvkn@users.noreply.github.com>
2025-02-18 21:45:27 +0300

    nvim: incline: render ft icon

--- nvim/dot-config/nvim/packs/incline.lua
+++ nvim/dot-config/nvim/packs/incline.lua
@@ -5,8 +5,25 @@ return {
       window = {
         margin = {
           vertical = 0,
+          horizontal = 0
         },
       },
+      render = function(props)
+        local helpers = require 'incline.helpers'
+        local devicons = require 'nvim-web-devicons'
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+        if filename == '' then
+          filename = '[No Name]'
+        end
+        local ft_icon, ft_color = devicons.get_icon_color(filename)
+        local modified = vim.bo[props.buf].modified
+        return {
+          ft_icon and { ' ', ft_icon, ' ', guifg = ft_color, --[[guifg = helpers.contrast_color(ft_color)]] } or '',
+          ' ',
+          { filename, gui = modified and 'bold,italic' or 'bold' },
+          ' ',
+        }
+      end,
     }
   end,
 }
