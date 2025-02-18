2c75fa25029855bb2003e29e8b3826735e9bf5a4
alxvkn <64454213+alxvkn@users.noreply.github.com>
2025-02-18 21:45:09 +0300

    nvim: remove neovide_refresh_rate setting

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -105,7 +105,6 @@ if vim.g.neovide then
   vim.g.neovide_cursor_vfx_mode = 'sonicboom'
   vim.g.neovide_cursor_animation_length = 0.15
   vim.g.neovide_cursor_trail_size = 0.3
-  vim.g.neovide_refresh_rate = 80
 end
 
 require 'lazy_bootstrap'
