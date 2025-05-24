87bdfba53c325fdb15711a0ad709e3fd8285082b
alxvkn <64454213+alxvkn@users.noreply.github.com>
2025-05-24 22:19:15 +0300

    nvim: update diagnostics icons config

--- nvim/dot-config/nvim/lua/diagnostics.lua
+++ nvim/dot-config/nvim/lua/diagnostics.lua
@@ -10,14 +10,15 @@
 
 vim.diagnostic.config({
   virtual_text = true,
-  signs = true,
   underline = true,
   update_in_insert = true,
   severity_sort = false,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "󰅚 ",
+      [vim.diagnostic.severity.WARN] = "󰀪 ",
+      [vim.diagnostic.severity.HINT] = "󰌶 ",
+      [vim.diagnostic.severity.INFO] = " ",
+    }
+  }
 })
-
-local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
