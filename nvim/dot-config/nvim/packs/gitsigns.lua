f2867e6cc6a598d9e0a4eb662d0205636b61ccae
alxvkn <64454213+alxvkn@users.noreply.github.com>
2025-06-06 00:08:45 +0300

    nvim: gitsigns: update bindings, make reset_buffer use capital R

--- nvim/dot-config/nvim/packs/gitsigns.lua
+++ nvim/dot-config/nvim/packs/gitsigns.lua
@@ -2,13 +2,12 @@ return {
   'lewis6991/gitsigns.nvim',
   config = function()
     local gitsigns = require('gitsigns')
-    vim.keymap.set('n', '[g', gitsigns.prev_hunk)
-    vim.keymap.set('n', ']g', gitsigns.next_hunk)
+    vim.keymap.set('n', '[g', function() gitsigns.nav_hunk('prev') end)
+    vim.keymap.set('n', ']g', function() gitsigns.nav_hunk('next') end)
     vim.keymap.set('n', '<Leader>gp', gitsigns.preview_hunk)
     vim.keymap.set('n', '<Leader>gs', gitsigns.stage_hunk)
-    vim.keymap.set('n', '<Leader>gu', gitsigns.undo_stage_hunk)
     vim.keymap.set('n', '<Leader>ga', gitsigns.stage_buffer)
-    vim.keymap.set('n', '<Leader>gr', gitsigns.reset_buffer)
+    vim.keymap.set('n', '<Leader>gR', gitsigns.reset_buffer)
     gitsigns.setup {
       signcolumn = true,
     }
