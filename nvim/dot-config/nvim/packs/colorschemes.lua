0f84488006cd48407d5637ef5b4dd5071b3732d3
alxvkn <64454213+alxvkn@users.noreply.github.com>
2025-06-10 23:51:51 +0300

    nvim: colorschemes

--- nvim/dot-config/nvim/packs/colorschemes.lua
+++ nvim/dot-config/nvim/packs/colorschemes.lua
@@ -2,29 +2,6 @@ return {
   'RRethy/nvim-base16',
   'kdheepak/monochrome.nvim',
   'ellisonleao/gruvbox.nvim',
-  -- 'ayu-theme/ayu-vim',
-  -- 'projekt0n/github-nvim-theme',
-  -- 'ramojus/mellifluous.nvim',
-  {
-    'Mofiqul/vscode.nvim',
-    lazy = true,
-    config = function()
-      require('vscode').setup {}
-    end
-  },
   'rose-pine/neovim',
   'vague2k/vague.nvim',
-  'rjshkhr/shadow.nvim',
-  -- {
-  --   'EdenEast/nightfox.nvim',
-  --   lazy = true,
-  --   config = function()
-  --     require('nightfox').setup {
-  --       options = {
-  --         dim_inactive = true,
-  --         transparent = false
-  --       }
-  --     }
-  --   end
-  -- },
 }
