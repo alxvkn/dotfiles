827104f2a3743eb1d84736bf64253546f702acd4
alxvkn <64454213+alxvkn@users.noreply.github.com>
2025-08-11 02:03:07 +0300

    nvim: update nerdy.nvim config

--- nvim/dot-config/nvim/packs/nerdy.lua
+++ nvim/dot-config/nvim/packs/nerdy.lua
@@ -1,7 +1,5 @@
 return {
   '2KAbhishek/nerdy.nvim',
-  keys = '<Leader>.',
-  config = function()
-    vim.keymap.set('n', '<Leader>.', require('nerdy').list)
-  end
+  keys = '<Leader>in',
+  config = true,
 }
