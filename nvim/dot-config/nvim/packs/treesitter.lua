81a777e03c43224f0e197e90fe4a32c90656378f
alxvkn <64454213+alxvkn@users.noreply.github.com>
2026-01-27 06:30:22 +0300

    nvim: rewrite treesitter textobjects config

--- nvim/dot-config/nvim/packs/treesitter.lua
+++ nvim/dot-config/nvim/packs/treesitter.lua
@@ -4,77 +4,87 @@ return {
     'nvim-treesitter/nvim-treesitter',
     branch = 'main',
     build = ':TSUpdate',
-    config = function()
-      require('nvim-treesitter.config').setup {
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      }
-    end,
   },
   {
     'nvim-treesitter/nvim-treesitter-textobjects',
     branch = 'main',
     config = function()
-      require('nvim-treesitter.config').setup {
-        textobjects = {
-          select = {
-            enable = true,
-
-            lookahead = true,
-            keymaps = {
-              ['aa'] = '@parameter.outer',
-              ['ia'] = '@parameter.inner',
-
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-
-              ['aC'] = '@call.outer',
-              ['iC'] = '@call.inner',
-
-              ['al'] = '@loop.outer',
-              ['il'] = '@loop.inner',
-
-              ['a='] = '@assignment.outer',
-              ['i='] = '@assignment.inner',
-              ['ik'] = '@assignment.lhs',
-              ['iv'] = '@assignment.rhs',
-
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-            },
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ["<leader>a"] = "@parameter.inner",
-            },
-            swap_previous = {
-              ["<leader>A"] = "@parameter.inner",
-            },
-          },
-          move = {
-            enable = true,
-            -- TODO: jumps to objects from select
-            goto_next_start = {
-              ["]f"] = "@function.outer",
-              ["]c"] = "@class.outer",
-            },
-            goto_next_end = {
-              ["]F"] = "@function.outer",
-              ["]C"] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[f"] = "@function.outer",
-              ["[c"] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[F"] = "@function.outer",
-              ["[C"] = "@class.outer",
-            },
-          },
-        }
+      require('nvim-treesitter-textobjects').setup {
+        select = {
+          lookahead = true,
+        },
       }
+
+      local select_keymaps = {
+        ['aa'] = '@parameter.outer',
+        ['ia'] = '@parameter.inner',
+
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+
+        ['aC'] = '@call.outer',
+        ['iC'] = '@call.inner',
+
+        ['al'] = '@loop.outer',
+        ['il'] = '@loop.inner',
+
+        ['a='] = '@assignment.outer',
+        ['i='] = '@assignment.inner',
+        ['ik'] = '@assignment.lhs',
+        ['iv'] = '@assignment.rhs',
+
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      }
+
+      local tto_select = require 'nvim-treesitter-textobjects.select'
+      for k, v in pairs(select_keymaps) do
+        vim.keymap.set(
+          { 'x', 'o' },
+          k,
+          function()
+            tto_select.select_textobject(v)
+          end
+        )
+      end
+
+      local tto_swap = require 'nvim-treesitter-textobjects.swap'
+      vim.keymap.set('n', '<Leader>a', function()
+        tto_swap.swap_next '@parameter.inner'
+      end)
+      vim.keymap.set('n', '<Leader>A', function()
+        tto_swap.swap_previous '@parameter.inner'
+      end)
+
+      local tto_move = require 'nvim-treesitter-textobjects.move'
+      local mode = { 'n', 'x', 'o' }
+      vim.keymap.set(mode, ']f', function()
+        tto_move.goto_next_start('@function.outer')
+      end)
+      vim.keymap.set(mode, ']c', function()
+        tto_move.goto_next_start('@class.outer')
+      end)
+
+      vim.keymap.set(mode, '[f', function()
+        tto_move.goto_previous_start('@function.outer')
+      end)
+      vim.keymap.set(mode, '[c', function()
+        tto_move.goto_previous_start('@class.outer')
+      end)
+
+      vim.keymap.set(mode, ']F', function()
+        tto_move.goto_next_end('@function.outer')
+      end)
+      vim.keymap.set(mode, ']C', function()
+        tto_move.goto_next_end('@class.outer')
+      end)
+
+      vim.keymap.set(mode, '[F', function()
+        tto_move.goto_previous_end('@function.outer')
+      end)
+      vim.keymap.set(mode, '[C', function()
+        tto_move.goto_previous_end('@class.outer')
+      end)
     end
   },
 }
