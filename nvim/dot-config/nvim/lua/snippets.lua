96b27a91d192844f426d6aa7d1177df9d181ad6f
alxvkn <64454213+alxvkn@users.noreply.github.com>
2026-02-10 16:53:58 +0300

    nvim: new snippets

--- nvim/dot-config/nvim/lua/snippets.lua
+++ nvim/dot-config/nvim/lua/snippets.lua
@@ -1,4 +1,6 @@
 local ls = require('luasnip')
+local fmt = require('luasnip.extras.fmt').fmt
+local fmta = require('luasnip.extras.fmt').fmta
 local s = ls.snippet
 local s = ls.snippet
 local isn = ls.indent_snippet_node
@@ -10,30 +12,43 @@ local c = ls.choice_node
 local d = ls.dynamic_node
 local r = ls.restore_node
 
-local c_cpp_main = s('main', {
-  t('int main(int argc, char** argv) {'),
-  t({ '', '    ' }), i(0),
-  t({ '', '    return 0;' }),
-  t({ '', '}' }),
-})
+local c_cpp_main = s('main',
+  fmta([[
+    int main(int argc, char** argv) {
+      <>
+      return 0;
+    }
+  ]],
+    { i(0) },
+    { indent_string = '  ' })
+)
 
 return {
+  all = {
+    s(
+      'fname',
+      f(function()
+        return vim.fn.expand '%:t'
+      end)
+    )
+  },
   c = {
     c_cpp_main
   },
   cpp = {
     c_cpp_main,
-    s('cout', {
-      t('std::cout << '),
-      i(1),
-      t(' << std::endl;'),
-    }),
+    s('cout',
+      fmt(
+        'std::cout << {} << std::endl;',
+        { i(1) }
+      )),
   },
   html = {
-    s('lcss', {
-      t('<link rel="stylesheet" href="'),
-      i(1),
-      t('">')
-    })
+    s('lcss',
+      fmt(
+        '<link rel="stylesheet" href="{}">',
+        { i(1) }
+      )
+    ),
   }
 }
