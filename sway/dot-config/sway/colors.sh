ef3dae4deddf2c7a8a3c652258a49d0097f12c32
alxvkn <64454213+alxvkn@users.noreply.github.com>
2026-02-13 16:42:11 +0300

    sway: overhaul colors.sh

--- sway/dot-config/sway/colors.sh
+++ sway/dot-config/sway/colors.sh
@@ -5,28 +5,36 @@ bg=#000000
 fg=#ffffff
 inactive_fg=#555555
 accent=#ffaaff
-wallpaper=~/Pictures/fefe8b37374b206d9a9da1cb9dbbd0f9.jpg
+wallpaper_dark=~/Pictures/arch_logo_white_on_black.png
+wallpaper_light=~/Pictures/arch_logo_black_on_white.png
 
-SCHEME=d
+SCHEME=dark
 
 if [ "$(gsettings get org.gnome.desktop.interface color-scheme)" = "'prefer-light'" ]; then
-    SCHEME=l
+    SCHEME=light
     _bg=$bg
     bg=$fg
     fg=$_bg
 fi
 
+if [ $SCHEME = 'dark' ]; then
+    WP=$wallpaper_dark
+elif [ $SCHEME = 'light' ]; then
+    WP=$wallpaper_light
+fi
+
 # TODO: swaylock
-swaymsg -- set \$lock swaylock -i $wallpaper -s center 2>&1 > /dev/null
+swaymsg -- set \$lock swaylock $([ -n "$WP" ] && echo "-i $WP") -c "${bg###}" --ring-color "${inactive_fg###}" > /dev/null
 
 killall swaybg
-swaybg -m center -c $bg -i $wallpaper > /dev/null & disown
+swaybg -m fill -c "$bg" -i "$WP" > /dev/null &
+# swaybg -c "$bg" > /dev/null &
 
 {
     #       class                    border  bg      text    indicator   child_border
-    swaymsg "client.focused          $fg     $bg     $fg     #aaaaaa     #aaaaaa"
+    swaymsg "client.focused          #ffffff $bg     $fg     #aaaaaa     $fg"
     swaymsg "client.focused_inactive #000000 $bg     #aaaaaa"
-    swaymsg "client.unfocused        #000000 $bg     #aaaaaa             #000000"
+    swaymsg "client.unfocused        #000000 $bg     #aaaaaa $accent     $bg"
 
 
     swaymsg bar main colors "background          $bg"
@@ -37,4 +45,4 @@ swaybg -m center -c $bg -i $wallpaper > /dev/null & disown
     swaymsg bar main colors "focused_workspace   $fg             $fg     $bg"
     swaymsg bar main colors "inactive_workspace  $bg             $bg     $inactive_fg"
     swaymsg bar main colors "urgent_workspace    $inactive_fg    $bg     #ffaaaa"
-} 2>&1 > /dev/null
+} > /dev/null 2>&1
