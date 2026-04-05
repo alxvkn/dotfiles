a693b98ac4b347910e0a6235278846ba84f89ee2
alxvkn <64454213+alxvkn@users.noreply.github.com>
2026-04-05 19:46:24 +0300

    nvim: don't change highlight of nvim_context_vt

--- nvim/dot-config/nvim/lua/plugins/nvim_context_vt.lua
+++ nvim/dot-config/nvim/lua/plugins/nvim_context_vt.lua
@@ -1,12 +1,9 @@
 ---@type LazySpec
 return {
   'https://github.com/andersevenrud/nvim_context_vt',
-  config = function()
-    vim.cmd[[hi ContextVt ctermbg=black ctermfg=gray guibg=black guifg=gray cterm=italic gui=italic]]
-    require('nvim_context_vt').setup {
-      prefix = '󱞿',
-      disable_virtual_lines = true,
-      min_rows = 10,
-    }
-  end
+  opts = {
+    prefix = '󱞿',
+    disable_virtual_lines = true,
+    min_rows = 10,
+  }
 }
