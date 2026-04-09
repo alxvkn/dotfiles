4cd78b0623d331a111cd77da7f15bc835f8aa554
alxvkn <64454213+alxvkn@users.noreply.github.com>
2026-04-09 14:04:59 +0300

    nvim12: diagnostics customizations

--- nvim12/dot-config/nvim12/plugin/lspconfig.lua
+++ nvim12/dot-config/nvim12/plugin/lspconfig.lua
@@ -67,6 +67,49 @@ local augroup = vim.api.nvim_create_augroup('my.lsp_setup', {})
 vim.api.nvim_create_autocmd('LspAttach', {
   group = augroup,
   callback = function()
+    local signs = {
+      [vim.diagnostic.severity.ERROR] = '󰚌',
+      [vim.diagnostic.severity.WARN] = '󰀪',
+      [vim.diagnostic.severity.INFO] = '󰋽',
+      [vim.diagnostic.severity.HINT] = '󰌶',
+    }
+
     vim.o.signcolumn = 'yes'
+    vim.diagnostic.config({
+      signs = { text = signs },
+      float = {
+        severity_sort = true,
+      },
+      virtual_text = {
+        current_line = false,
+        prefix = '',
+      },
+      severity_sort = true,
+      jump = { float = true },
+    })
+
+    vim.keymap.set('n', 'K', function()
+      local d = vim.diagnostic.get_next { count = 1 }
+      if d == nil then
+        vim.lsp.buf.hover()
+        return
+      end
+
+      local cursor = vim.api.nvim_win_get_cursor(0)
+      local c = {
+        line = cursor[1],
+        col = cursor[2],
+      }
+
+      if c.line >= d.lnum + 1 and c.line <= d.end_lnum + 1
+        and
+        c.col >= d.col and c.col <= d.end_col
+        then
+        vim.diagnostic.open_float()
+      else
+        vim.lsp.buf.hover()
+      end
+    end, { buf = 0 })
+
   end
 })
