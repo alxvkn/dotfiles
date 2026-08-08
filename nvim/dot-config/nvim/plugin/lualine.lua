4482c91717e8199e6a31a2b57860670b2dd2d45d
alxvkn <64454213+alxvkn@users.noreply.github.com>
2026-08-09 01:50:21 +0300

    nvim: tweak lualine

--- nvim/dot-config/nvim/plugin/lualine.lua
+++ nvim/dot-config/nvim/plugin/lualine.lua
@@ -1,6 +1,24 @@
 vim.pack.add { 'https://github.com/nvim-lualine/lualine.nvim' }
 
 require('lualine').setup {
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+      },
+      '%=',
+      'lsp_status'
+    },
+    lualine_y = {},
+    lualine_z = {
+      function ()
+        return ('(%d/%d):(%d/%d)'):format(
+          vim.fn.line('.'), vim.fn.line('$'),
+          vim.fn.virtcol('.'), vim.fn.strchars(vim.fn.getline('.')))
+      end,
+    },
+  },
   options = {
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
