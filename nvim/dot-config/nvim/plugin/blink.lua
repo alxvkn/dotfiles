43f79850ab98ed0cafeb689ac81aa191c64a03ff
alxvkn <64454213+alxvkn@users.noreply.github.com>
2026-08-09 01:45:51 +0300

    nvim: try to lazy load blink

--- nvim/dot-config/nvim/plugin/blink.lua
+++ nvim/dot-config/nvim/plugin/blink.lua
@@ -4,30 +4,36 @@
 --   callback = function(e)
 --     local name, kind = e.data.spec.name, e.data.kind
 --
---     if name == 'blink.cmp' and kind == 'install' or kidn == 'update' then
+--     if name == 'blink.cmp' and kind == 'install' or kind == 'update' then
 --       vim.system({ 'cargo', 'build', '--release' }, { cwd = e.data.path }):wait()
 --     end
 --   end
 -- })
 
-vim.pack.add { 'https://github.com/saghen/blink.cmp' }
+vim.api.nvim_create_autocmd({ 'InsertEnter', 'CmdLineEnter' }, {
+  group = vim.api.nvim_create_augroup('my.blink', {}),
+  once = true,
+  callback = function()
+    vim.pack.add { 'https://github.com/saghen/blink.cmp' }
 
-require('blink.cmp').setup {
-  keymap = {
-    preset = 'super-tab',
-    ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
-    ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
-  },
-  signature = { enabled = true },
-  completion = {
-    -- menu = { auto_show = false },
-    -- ghost_text = {
-    --   enabled = true,
-    --   show_with_menu = false,
-    -- },
-    documentation = {
-      auto_show = true
+    require('blink.cmp').setup {
+      keymap = {
+        preset = 'super-tab',
+        ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+        ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+      },
+      signature = { enabled = true },
+      completion = {
+        -- menu = { auto_show = false },
+        -- ghost_text = {
+        --   enabled = true,
+        --   show_with_menu = false,
+        -- },
+        documentation = {
+          auto_show = true
+        }
+      },
+      fuzzy = { implementation = 'lua' }
     }
-  },
-  fuzzy = { implementation = 'lua' }
-}
+  end
+})
