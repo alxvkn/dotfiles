8d632ef250a4341333fb029471c4019709e4d21b
alxvkn <64454213+alxvkn@users.noreply.github.com>
2026-08-09 01:48:39 +0300

    nvim: lazy load zen-mode

--- nvim/dot-config/nvim/plugin/zen-mode.lua
+++ nvim/dot-config/nvim/plugin/zen-mode.lua
@@ -1,6 +1,13 @@
-vim.pack.add { 'https://github.com/folke/zen-mode.nvim' }
+local zen = nil
 
-local zen = require('zen-mode')
-zen.setup {}
+local function load_or_forward_toggle()
+  if not zen then
+    vim.pack.add { 'https://github.com/folke/zen-mode.nvim' }
+    zen = require('zen-mode')
+    zen.setup()
+  end
 
-vim.keymap.set('n', '<Leader>z', zen.toggle)
+  zen.toggle()
+end
+
+vim.keymap.set('n', '<Leader>z', load_or_forward_toggle)
