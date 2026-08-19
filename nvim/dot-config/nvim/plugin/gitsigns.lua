20a065d24ed5ee4f07af91ef84546d4e0cb9ad4c
alxvkn <64454213+alxvkn@users.noreply.github.com>
2026-08-19 03:25:28 +0300

    nvim: add gitsigns keymaps and hunk textobject

--- nvim/dot-config/nvim/plugin/gitsigns.lua
+++ nvim/dot-config/nvim/plugin/gitsigns.lua
@@ -2,10 +2,25 @@ vim.pack.add { 'https://github.com/lewis6991/gitsigns.nvim' }
 
 local gs = require('gitsigns')
 
-gs.setup()
+gs.setup {
+  on_attach = function(buf)
+    local opts = { buf = buf }
 
-vim.keymap.set('n', ']g', function() gs.nav_hunk('next') end)
-vim.keymap.set('n', '[g', function() gs.nav_hunk('prev') end)
+    vim.keymap.set('n', ']g', function() gs.nav_hunk('next') end, opts)
+    vim.keymap.set('n', '[g', function() gs.nav_hunk('prev') end, opts)
 
-vim.keymap.set('n', ']G', function() gs.nav_hunk('last') end)
-vim.keymap.set('n', '[G', function() gs.nav_hunk('first') end)
+    vim.keymap.set('n', ']G', function() gs.nav_hunk('last') end, opts)
+    vim.keymap.set('n', '[G', function() gs.nav_hunk('first') end, opts)
+
+    vim.keymap.set('n', '<Leader>gp', function() gs.preview_hunk() end, opts)
+    vim.keymap.set('n', '<Leader>gd', function() gs.diffthis() end, opts)
+
+    vim.keymap.set('n', '<Leader>gb', function() gs.blame_line() end, opts)
+
+    vim.keymap.set('n', '<Leader>ga', function() gs.stage_hunk() end, opts)
+    vim.keymap.set('n', '<Leader>gu', function() gs.undo_stage_hunk() end, opts)
+
+    vim.keymap.set({ 'x', 'o' }, 'ag', function() gs.select_hunk() end, opts)
+    vim.keymap.set({ 'x', 'o' }, 'ig', function() gs.select_hunk() end, opts)
+  end
+}
